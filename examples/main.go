e0ab162a2d1755634ccf5f382e401869b20b1cd4
Gustavo Teixeira <gustavofigueiredoteixeira8@gmail.com>
2023-03-17 07:13:34 -0300

    update examples

--- examples/main.go
+++ examples/main.go
@@ -10,15 +10,15 @@ import (
 func main() {
 	c := cron.New()
 	//          s  m  h  d  m  y
-	c.AddFunc("20 40 08 16 03 2023", nil, func() error {
+	c.AddFunc("* * 07 17 03 2023", nil, func() error {
 		fmt.Printf("Olá mundo 1: %v\n", time.Now())
 		return nil
 	})
 
-	c.AddFunc("* * * * * *", nil, func() error {
-		fmt.Printf("Olá mundo 2: %v\n", time.Now())
-		return nil
-	})
+	// c.AddFunc("* * * * * *", nil, func() error {
+	// 	fmt.Printf("Olá mundo 2: %v\n", time.Now())
+	// 	return nil
+	// })
 
 	c.Start()
 }
